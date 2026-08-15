aa93a4d705ba3f1652ee1583fbef10cfd207be2e
José Guerra <guerracerdaj@gmail.com>
2026-08-15 12:25:06 -0400

    Aprendiendo operadores de comparación y aritmeticos

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,18 @@
+#suma y resta (+ y -)
+suma = 5 + 5
+resta = 5 - 4
+
+#multiplicación y división (* y /)
+multi = 2 * 2
+division = 4 / 2 #devuelve dato float (flotante) dato con decimal
+
+#potenciación (exponente) (**)
+exponente = 2**2
+
+#división baja (//)
+division_baja = 12//5
+
+#resto o módulo
+resto = 12 % 5
+
+print(exponente)
---
+++ operadores/op_comparacion.py
@@ -0,0 +1,27 @@
+#Los operadores de comparación nos devuelve o (True or False)
+
+es_igual_que = 5 == 6
+
+es_distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+#calculos combinados
+
+a = 5
+b = 3
+c = 15
+
+comparacion = a * b == c
+
+#comparacion de datos
+contraseña_almacenada = "Lola123"
+contraseña_escrita = "juanito123"
+
+print(contraseña_almacenada != contraseña_escrita)
