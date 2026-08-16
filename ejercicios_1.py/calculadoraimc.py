009ff41cfa898314d0a7e251586dff8bfa8848ff
José Guerra <guerracerdaj@gmail.com>
2026-08-15 21:32:00 -0400

    Actualización de IMC

--- ejercicios_1.py/calculadoraimc.py
+++ ejercicios_1.py/calculadoraimc.py
@@ -1,6 +1,6 @@
 #Calculadora de IMC
 
-print("Calculadora IMC")
+print("Calculadora IMC: Índice de Masa Corporal")
 peso = float(input("Ingresa tu Peso (KG): "))
 altura = float(input("Ingresa tu Altura (M): "))
 imc = peso / altura**2
@@ -8,9 +8,9 @@ imc = peso / altura**2
 if imc:
     if imc >= 30:
         print(f"Tienes Obesidad | IMC:{imc}")
-    elif imc >= 25 <= 29.9:
+    elif imc >= 25:
         print(f"Tienes Sobrepeso | IMC:{imc}") 
-    elif imc >= 18.5 <= 24.9:
+    elif imc >= 18.5:
         print(f"Tienes Peso normal | IMC:{imc}")
-    elif imc < 18.5:
+    else:
         print(f"Tienes Bajo peso | IMC:{imc}")        
