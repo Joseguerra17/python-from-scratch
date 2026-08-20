f863d84689efbf5f70b4803460dd67e37fac0048
José Guerra <guerracerdaj@gmail.com>
2026-08-19 23:22:37 -0400

    Creando diferentes funciones

--- funciones/crear_funciones.py
+++ funciones/crear_funciones.py
@@ -29,18 +29,53 @@
 #saludar(input("Cual es tu nombre? "))
 
 #creando una funcion que devuelva valores
-def crear_contraseña_random(num):
-    chars = "akjfomiowfaoi"
-    num_entero = str(num)
-    num = int(num_entero[0])
-    c1 = num - 2
-    c2 = num
-    c3 = num - 5
-    contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}"
-    return contraseña,num
+#def crear_contraseña_random(num):
+    #chars = "akjfomiowfaoi"
+    #num_entero = str(num)
+    #num = int(num_entero[0])
+    #c1 = num - 2
+    #c2 = num
+    #c3 = num - 5
+    #contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}"
+    #return contraseña,num
 
-password,primer_numero = crear_contraseña_random(9)
+#password,primer_numero = crear_contraseña_random(9)
 
-print(f"Tu Contraseña nueva es: {password}")
-print(f"El numero utilizado para crearla fue: {primer_numero}")
+#print(f"Tu Contraseña nueva es: {password}")
+#print(f"El numero utilizado para crearla fue: {primer_numero}")
+
+
+#def clasificador_temperaturas(grados):
+    #if grados < 10:
+        #print(f"Hace demasiado Frío en tu ciudad: {grados}°C")
+    #elif grados <= 24:
+        #print(f"Actualmente hay un clima templado en tu ciudad: {grados}°C")
+    #else:
+        #print(f"Hace demasiada calor en tu ciudad: {grados}°C")
+    #return grados
+
+#grados = clasificador_temperaturas(19)
+
+#def clasificador_temperaturas(grados):
+   # if (grados < 10):
+       # return f"Hace demasiado Frío en tu ciudad: {grados}°C"
+    #elif (grados < 24):
+        #return f"Actualmente hay un clima templado en tu ciudad: {grados}°C"
+    #else:
+        #return f"Hace demasiada calor en tu ciudad: {grados}°C"
+
+#resultado = clasificador_temperaturas(9)
+
+#if "templado" in resultado.lower():
+    #print("¡Buen Dia para Salir!")
+#if "frío" in resultado.lower():
+    #print("¡Hace demasiado Frío el dia de Hoy!")
+        
+#print(resultado)
+
+def filtrador_notas(notas):
+    notas = list
     
+    
+resultado = filtrador_notas([65,69,67])
+print(resultado)
